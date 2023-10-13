1ee075ccee7694cf5a14407ca695ac3a0a630147
donsonioc2010 <whddnjs822@gmail.com>
2023-10-13 14:10:54 +0900

    feat : 공통기능 추가 - Null유틸리티 메소드 추가 - Footer완료~ - Logo제작 및 추가~ - Cookie Provier 미사용으로 인한 제거

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { CookiesProvider } from "react-cookie";
+
+import "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CookiesProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </CookiesProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
--- src/utils/NullUtils.jsx
+++ src/utils/NullUtils.jsx
@@ -5,3 +5,11 @@ export const isNullReturnString = (data) => {
 export const isNullReturnString2 = (data, returnString) => {
   return data === null || data === undefined ? "" : returnString;
 };
+
+export const isNull = (data) => {
+  return data === null || data === undefined;
+};
+
+export const isNotNull = (data) => {
+  return !data === null || !data === undefined;
+};
