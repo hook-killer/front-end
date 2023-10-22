dc769ef57457e6ca74aeba359827d1826e6fe082
lljh1992 <lljh1992@naver.com>
2023-10-22 20:47:19 +0900

    style : 코드수정
    .

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -14,8 +14,6 @@ import { useTranslation } from "react-i18next";
 const SelectLanguage = (props) => {
   const lang = props.language;
   const setLanguage = props.languageSet;
-  const setToken = props.tokenSet;
-  const setRole = props.roleSet;
   const { t, i18n } = useTranslation();
 
   const languageData = [
@@ -190,9 +188,7 @@ const LoginMenu = (props) => {
  */
 
 const MemberArea = (props) => {
-  let token = props.token;
   let role = props.role;
-  console.log(props);
   if (role === "GUEST") {
     return NotLoginMenu();
   }
